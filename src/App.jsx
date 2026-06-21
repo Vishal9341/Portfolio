ce423e0de2b2ef51b05f6bfd63a37e4f78461b53
Vishal Kumar <vishalcool9341@gmail.com>
2026-06-21 15:17:46 +0530

    Little change in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div >
-      <p className='bg-amber-500'>Hy I am Vishal Kumar </p>
+      <p className='bg-amber-500'>Hy I am Vishal Kumar and I am from Bihar </p>
     </div>
   )
 }
